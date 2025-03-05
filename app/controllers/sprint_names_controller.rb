415e5fae5efb9217084bc71e36b69c558650874f
Wihan Nel <wnel@predictiveindex.com>
2025-03-05 17:45:44 -0500

    basics

---
+++ app/controllers/sprint_names_controller.rb
@@ -0,0 +1,34 @@
+# app/controllers/sprint_names_controller.rb
+class SprintNamesController < ApplicationController
+  def generate
+    current_date = Date.parse(params[:date] || Date.today.to_s)
+    sprint_letter = calculate_sprint_letter(current_date)
+    sprint_name = generate_sprint_name(sprint_letter)
+    render json: { sprint_letter: sprint_letter, sprint_name: sprint_name }
+  end
+
+  private
+
+  def calculate_sprint_letter(date)
+    start_date = Date.new(2025, 1, 1)
+    days_since_start = (date - start_date).to_i
+    sprint_number = (days_since_start / 14) + 1
+    ("A".."Z").to_a[sprint_number - 1] || "Z" # Cap at Z
+  end
+
+  def generate_sprint_name(letter)
+    attempts = 0
+    max_attempts = 50
+
+    # Use Faker to generate a name starting with the given letter
+    loop do
+      name = Faker::Company.buzzword # Or another Faker category
+      return "#{name.capitalize}" if name.start_with?(letter)
+
+      attempts += 1
+      if attempts >= max_attempts
+        return "Sprint-#{letter.downcase}" # Fallback when no match found
+      end
+    end
+  end
+end
